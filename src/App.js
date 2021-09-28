d46d2577b38d3265f8f5f46ce01731e14417a107
ramchandra <ramchandrasharma1995@gmail.com>
2021-09-28 19:31:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import App from "../src/App.css";
+import Navbar from "./Component/Navbar";
+import Users from "./Component/Users";
+function App1() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Users />
+    </div>
+  );
+}
+
+export default App1;
---
+++ src/Component/Users.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+
+const Users = () => {
+    return (
+        <div>
+           <table class="content-table">
+  <thead>
+    <tr>
+      <th>SL.No</th>
+      <th>Name</th>
+      <th>Company</th>
+      <th>Email</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Ram</td>
+      <td>Stackaero</td>
+      <td>ram@gmail.com</td>
+      
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Prem</td>
+      <td>Stackaero</td>
+      <td>Prem@gmail.com</td>
+    
+    </tr>
+    <tr>
+    <td>3</td>
+      <td>Hari</td>
+      <td>Stackaero</td>
+      <td>Hari@gmail.com</td>
+        
+    </tr>
+    <tr >
+      <td>4</td>
+      <td>Sabitha </td>
+      <td>Stackaero</td>
+      <td>Sabitha@gmail.com</td>
+    
+    </tr>
+  </tbody>
+</table>
+        </div>
+    )
+}
+
+export default Users
